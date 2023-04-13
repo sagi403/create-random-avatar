c0282bd79588c3f9204ade92dc1625f0f0f08739
Sagi <sagi403@gmail.com>
2023-04-13 12:21:47 +0300

    Adding basename props for the BrowserRouter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MainScreen from "./screens/MainScreen";
 const App = () => {
   return (
     <>
-      <Router>
+      <Router basename="/create-random-avatar">
         <Routes>
           <Route path="/" element={<MainScreen />} />
         </Routes>
